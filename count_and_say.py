2becf22a711c86e4e96728a21d5c2f50fe2c1d93
Mrsyuwei <33003132+Mrsyuwei@users.noreply.github.com>
2018-03-01 17:34:37 +0800

    searchInsert

--- count_and_say.py
+++ count_and_say.py
@@ -13,20 +13,23 @@ class Solution(object):
                 if i==0:
                     first='1'
                 else:
-                    sum=0
-                    sum_num=first[0]
-                    new_num=''
-                    for j in range(len(first)):
-                        if sum_num==first[j]:
-                            sum=sum+1
-                        else:
-                            new_num = new_num + str(sum) + first[j-1]
-                            sum_num=first[j]
-                            sum=1
-                    new_num = new_num + str(sum) + first[j]
-                    first=new_num
+                    first=self.Single(first)
         return first
+    def Single(self,first):
+        sum = 0
+        sum_num = first[0]
+        new_num = ''
+        for j in range(len(first)):
+            if sum_num == first[j]:
+                sum = sum + 1
+            else:
+                new_num = new_num + str(sum) + first[j - 1]
+                sum_num = first[j]
+                sum = 1
+        new_num = new_num + str(sum) + first[j]
+        return new_num
 
-n=5
+
+n=4
 p=Solution()
 print p.countAndSay(n)
